ac19bd96133d56bfca1f70990c27112b36944abe
Jesse Zhuang <408455428@qq.com>
2016-04-01 15:51:58 +0800

    Update app.js

--- node/static/app.js
+++ node/static/app.js
@@ -1,7 +1,11 @@
 var http = require("http");
+var url = require("url");
 
 http.createServer(function(request, response) {
-
+  
+  var pathname = url.parse(request.url).pathname;
+  var realPath = "assets" + pathname;
+  
   response.writeHead(200, {"Content-Type": "text/plain"});
 
   response.write("Hello World");
